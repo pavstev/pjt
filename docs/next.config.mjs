13b9efcf91b0e5f7f17945bd6df2fc94424cdce8
Steve Paulson <staevage@gmail.com>
2025-12-03 06:40:52 +0100

    WIP

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,15 +1,14 @@
 import { createMDX } from 'fumadocs-mdx/next';
 
 const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'pjt';
 
 /** @type {import('next').NextConfig} */
 const config = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `https://${repoName}.stevanpavlovic.com/` : '',
+  basePath: '',
+  assetPrefix: isProd ? 'https://pjt.stevanpavlovic.com/' : '',
 };
 
 const withMDX = createMDX();
